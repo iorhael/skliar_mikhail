d7cd6c6a2bf25d727fca78b0a91ebc006d931ec3
Mikhail Skliar <skliarmikhail@gmail.com>
2025-01-20 02:11:18 +0300

    Remove custom DI framework

--- di/src/main/java/com/senla/di/exception/BeanPostProcessorRegistrationException.java
+++
@@ -1,7 +0,0 @@
-package com.senla.di.exception;
-
-public class BeanPostProcessorRegistrationException extends RuntimeException {
-    public BeanPostProcessorRegistrationException(String postProcessorType, String message) {
-        super(String.format("Unable to register post processor with type %s. %s", postProcessorType, message));
-    }
-}
