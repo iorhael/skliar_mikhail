02bb1a20710c15d2c88d933aba7f4b3e228606a1
Mikhail Skliar <skliarmikhail@gmail.com>
2025-01-24 18:26:39 +0300

    Configure web module with spring webmvc

--- service/src/main/java/com/senla/config/ServiceConfig.java
+++ service/src/main/java/com/senla/config/ServiceConfig.java
@@ -8,11 +8,13 @@ import com.senla.config.modelmapper.VoteMappingsConfigurator;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
 @Configuration
+@ComponentScan("com.senla.service")
 public class ServiceConfig {
 
     @Bean
