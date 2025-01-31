9ded48a6b83466dc7b35ebeaa2cc98b6347d500c
Mikhail Skliar <skliarmikhail@gmail.com>
2025-01-31 21:14:08 +0300

    Update controllers

--- web/src/main/java/com/senla/controller/CommentController.java
+++ web/src/main/java/com/senla/controller/CommentController.java
@@ -1,115 +1,66 @@
 package com.senla.controller;
 
+import com.senla.controller.dto.ResponseInfoDto;
 import com.senla.dto.comment.CommentCreateDto;
 import com.senla.dto.comment.CommentGetDto;
 import com.senla.dto.comment.CommentUpdateDto;
-import com.senla.service.CommentService;
-import com.senla.service.PostService;
-import com.senla.service.UserService;
+import com.senla.service.imp.CommentServiceImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
 
-@Controller
-@RequestMapping("/comment")
+@RestController
+@RequestMapping("/api/comments")
 @RequiredArgsConstructor
 public class CommentController {
-    private static final String LIST_PATH = "comment/list";
-    private static final String CREATE_PATH = "comment/create";
-    private static final String EDIT_PATH = "comment/edit";
-    private static final String LIST_REDIRECT_PATH = "redirect:/comment";
-    private static final String CREATE_REDIRECT_PATH = "redirect:/comment/new";
-    private static final String EDIT_REDIRECT_PATH = "redirect:/comment/edit?id=";
-    private static final String COMMENT_ATTRIBUTE_NAME = "comment";
-    private static final String COMMENTS_ATTRIBUTE_NAME = "comments";
-    private static final String POSTS_ATTRIBUTE_NAME = "posts";
-    private static final String AUTHORS_ATTRIBUTE_NAME = "authors";
+    private static final String COMMENT_DELETION_MESSAGE = "Comment deleted successfully";
 
-    private final CommentService commentService;
-
-    private final PostService postService;
-
-    private final UserService userService;
-
-    private final ModelMapper modelMapper;
+    private final CommentServiceImpl commentService;
 
     @GetMapping
-    public String showListForm(Model model) {
-        List<CommentGetDto> comments = commentService.getAllComments();
-        model.addAttribute(COMMENT_ATTRIBUTE_NAME, comments);
-
-        return LIST_PATH;
+    @ResponseStatus(HttpStatus.OK)
+    public List<CommentGetDto> findAllComments() {
+        return commentService.getAllComments();
     }
 
-    @GetMapping("/new")
-    public String showNewForm(Model model) {
-        if (!model.containsAttribute(COMMENT_ATTRIBUTE_NAME)) {
-            model.addAttribute(COMMENT_ATTRIBUTE_NAME, new CommentCreateDto());
-        }
-        model.addAttribute(POSTS_ATTRIBUTE_NAME, postService.getAllPosts());
-        model.addAttribute(AUTHORS_ATTRIBUTE_NAME, userService.getAllUsers());
-        model.addAttribute(COMMENTS_ATTRIBUTE_NAME, commentService.getAllComments());
-
-        return CREATE_PATH;
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CommentGetDto findCommentById(@PathVariable UUID id) {
+        return commentService.getCommentById(id);
     }
 
-    @PostMapping("/insert")
-    public String createComment(@Valid @ModelAttribute(COMMENT_ATTRIBUTE_NAME) CommentCreateDto comment,
-                                BindingResult bindingResult,
-                                RedirectAttributes redirectAttributes) {
-        if (bindingResult.hasErrors()) {
-            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.comment", bindingResult);
-            redirectAttributes.addFlashAttribute(COMMENTS_ATTRIBUTE_NAME, comment);
-            return CREATE_REDIRECT_PATH;
-        }
-
-        if (comment.getParentComment().getId() == null) comment.setParentComment(null);
-
-        commentService.createComment(comment);
-        return LIST_REDIRECT_PATH;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CommentGetDto createComment(@Valid @RequestBody CommentCreateDto comment) {
+        return commentService.createComment(comment);
     }
 
-    @GetMapping("/edit")
-    public String showEditForm(@RequestParam UUID id, Model model) {
-        if (!model.containsAttribute(COMMENT_ATTRIBUTE_NAME)) {
-            CommentUpdateDto comment = modelMapper.map(commentService.getCommentById(id), CommentUpdateDto.class);
-            model.addAttribute(COMMENT_ATTRIBUTE_NAME, comment);
-        }
-        model.addAttribute("id", id);
-
-        return EDIT_PATH;
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CommentGetDto updateComment(@Valid @RequestBody CommentUpdateDto comment,
+                                       @PathVariable UUID id) {
+        return commentService.updateComment(comment, id);
     }
 
-    @PostMapping("/update")
-    public String updateComment(@RequestParam UUID id,
-                                @Valid @ModelAttribute(COMMENT_ATTRIBUTE_NAME) CommentUpdateDto comment,
-                                BindingResult bindingResult,
-                                RedirectAttributes redirectAttributes) {
-        if (bindingResult.hasErrors()) {
-            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.comment", bindingResult);
-            redirectAttributes.addFlashAttribute(COMMENT_ATTRIBUTE_NAME, comment);
-            return EDIT_REDIRECT_PATH + id;
-        }
-
-        commentService.updateComment(comment, id);
-        return LIST_REDIRECT_PATH;
-    }
-
-    @GetMapping("/delete")
-    public String deleteComment(@RequestParam UUID id) {
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseInfoDto deleteComment(@PathVariable UUID id) {
         commentService.deleteComment(id);
-        return LIST_REDIRECT_PATH;
+
+        return ResponseInfoDto.builder()
+                .message(COMMENT_DELETION_MESSAGE)
+                .build();
     }
 }
--- web/src/main/java/com/senla/controller/UserController.java
+++ web/src/main/java/com/senla/controller/UserController.java
@@ -1,101 +1,71 @@
 package com.senla.controller;
 
+import com.senla.controller.dto.ResponseInfoDto;
 import com.senla.dto.user.UserCreateDto;
 import com.senla.dto.user.UserGetDto;
 import com.senla.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
 
-@Controller
-@RequestMapping("/user")
+@Slf4j
+@RestController
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
-    private static final String LIST_PATH = "user/list";
-    private static final String CREATE_PATH = "user/create";
-    private static final String EDIT_PATH = "user/edit";
-    private static final String LIST_REDIRECT_PATH = "redirect:/user";
-    private static final String CREATE_REDIRECT_PATH = "redirect:/user/new";
-    private static final String EDIT_REDIRECT_PATH = "redirect:/user/edit?id=";
-    private static final String USER_ATTRIBUTE_NAME = "user";
-    private static final String USERS_ATTRIBUTE_NAME = "users";
+    private static final String USER_DELETION_MESSAGE = "User deleted successfully";
 
     private final UserService userService;
 
-    private final ModelMapper modelMapper;
-
     @GetMapping
-    public String showListForm(Model model) {
-        List<UserGetDto> users = userService.getAllUsers();
-        model.addAttribute(USERS_ATTRIBUTE_NAME, users);
-
-        return LIST_PATH;
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserGetDto> findAllUsers() {
+        return userService.getAllUsers();
     }
 
-    @GetMapping("/new")
-    public String showNewForm(Model model) {
-        if (!model.containsAttribute(USER_ATTRIBUTE_NAME)) {
-            model.addAttribute(USER_ATTRIBUTE_NAME, new UserCreateDto());
-        }
-
-        return CREATE_PATH;
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserGetDto findUserById(@PathVariable UUID id) {
+        return userService.getUserById(id);
     }
 
-    @PostMapping("/insert")
-    public String createUser(@Valid @ModelAttribute(USER_ATTRIBUTE_NAME) UserCreateDto user,
-                             BindingResult bindingResult,
-                             RedirectAttributes redirectAttributes) {
-        if (bindingResult.hasErrors()) {
-            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.user", bindingResult);
-            redirectAttributes.addFlashAttribute(USER_ATTRIBUTE_NAME, user);
-            return CREATE_REDIRECT_PATH;
-        }
-
-        userService.createUser(user);
-        return LIST_REDIRECT_PATH;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserGetDto createUser(@Valid @RequestBody UserCreateDto user) {
+        return userService.createUser(user);
     }
 
-    @GetMapping("/edit")
-    public String showEditForm(@RequestParam UUID id, Model model) {
-        if (!model.containsAttribute(USER_ATTRIBUTE_NAME)) {
-            UserCreateDto user = modelMapper.map(userService.getUserById(id), UserCreateDto.class);
-            model.addAttribute(USER_ATTRIBUTE_NAME, user);
-        }
-        model.addAttribute("id", id);
-
-        return EDIT_PATH;
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserGetDto updateUser(@Valid @RequestBody UserCreateDto user,
+                                 @PathVariable UUID id) {
+        return userService.updateUser(user, id);
     }
 
-    @PostMapping("/update")
-    public String updateUser(@RequestParam UUID id,
-                             @Valid @ModelAttribute(USER_ATTRIBUTE_NAME) UserCreateDto user,
-                             BindingResult bindingResult,
-                             RedirectAttributes redirectAttributes) {
-        if (bindingResult.hasErrors()) {
-            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.user", bindingResult);
-            redirectAttributes.addFlashAttribute(USER_ATTRIBUTE_NAME, user);
-            return EDIT_REDIRECT_PATH + id;
-        }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseInfoDto deleteUser(@PathVariable UUID id) {
+        log.info("The user annihilation process has started...");
 
-        userService.updateUser(user, id);
-        return LIST_REDIRECT_PATH;
-    }
-
-    @GetMapping("/delete")
-    public String deleteUser(@RequestParam UUID id) {
         userService.deleteUser(id);
-        return LIST_REDIRECT_PATH;
+
+        log.info("Farewell {}", id);
+
+        return ResponseInfoDto.builder()
+                .message(USER_DELETION_MESSAGE)
+                .build();
     }
 }
